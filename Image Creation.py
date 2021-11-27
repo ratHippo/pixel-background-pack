148569578a89577875316a3b1af85ea82b44d05e
ratHippo <91836375+ratHippo@users.noreply.github.com>
2021-11-27 19:02:10 +0000

    Add files via upload

--- Image Creation.py
+++ Image Creation.py
@@ -2,7 +2,7 @@ import colors,random
 from PIL import Image, ImageOps
 width = 48
 height = 30
-palette = ["c98686","81a79d","8ac1e2","8fd6f5","dce0e4"] #Sea Wolf Rev 2
+palette = ["c98686","81a79d","8ac1e2","8fd6f5","dce0e4"] #Sea Wolf Rev 2, example pallete
 img  = Image.new( mode = "RGB", size = (width, height) )
 pixels = img.load()
 for i in range(width):
